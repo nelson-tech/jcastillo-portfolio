e361f000f23ec20903ace136a4da805e89142ba6
Michael Nelson <revnelson@gmail.com>
2023-04-19 03:01:43 +0000

    Add FooterIcon to directus Collections typings.

--- astro/src/utils/types/directus.ts
+++ astro/src/utils/types/directus.ts
@@ -1,11 +1,22 @@
 import type { ID } from "@directus/sdk";
 
+type Status = {
+  status: "draft" | "published";
+};
+
 type MenuItem = {
   id: ID;
   label: string;
   path: string;
-};
+} & Status;
+
+type FooterIcon = {
+  id: ID;
+  brand: string;
+  link: string;
+} & Status;
 
 export type Collections = {
   menu_items: MenuItem;
+  footer_icons: FooterIcon;
 };
