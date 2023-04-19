d4cb7078ab4dbbaf9cf02e5c7eac1ac3c9032fe8
Michael Nelson <revnelson@gmail.com>
2023-04-19 03:00:40 +0000

    Add tailwind integration to astro.

---
+++ astro/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        highlight: "#5592b5",
+      },
+    },
+  },
+  plugins: [],
+};
