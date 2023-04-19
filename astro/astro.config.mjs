74ce6908a96399a296555714948096e70e4f7282
Michael Nelson <revnelson@gmail.com>
2023-04-19 03:01:13 +0000

    Add logo and homepage images.

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  server: { port: 3333, host: true },
+  server: {
+    port: 3333,
+    host: true
+  },
+  integrations: [tailwind()]
 });
