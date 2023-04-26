d3be2534e033b27b2d541a7c5d7be144f4970b7f
Michael Nelson <revnelson@gmail.com>
2023-04-26 07:45:40 +0000

    Modify MenuButton component.

--- astro/src/components/MenuButton.svelte
+++ astro/src/components/MenuButton.svelte
@@ -6,23 +6,22 @@
   };
 </script>
 
-<div
+<button
   id="menu-icon"
   class={`cursor-pointer z-50 ${$isMenuOpen ? "close" : ""}`}
   on:click={toggleMenu}
-  on:keypress={toggleMenu}
 >
   <div class="bar" />
   <div class="bar" />
   <div class="bar" />
-</div>
+</button>
 
 <style>
-  div#menu-icon {
+  button#menu-icon {
     display: inline-block;
     padding: 10px;
   }
-  div#menu-icon .bar {
+  button#menu-icon .bar {
     margin: 3px;
     width: 25px;
     height: 3px;
@@ -38,23 +37,23 @@
     transition: margin 0.2s, transform 0.2s, -webkit-transform 0.2s;
   }
 
-  div#menu-icon:not(.close):hover {
+  button#menu-icon:not(.close):hover {
     padding: 6px 10px;
   }
 
-  div#menu-icon:not(.close):hover .bar {
+  button#menu-icon:not(.close):hover .bar {
     margin: 5px 3px;
   }
 
-  div#menu-icon.close .bar:first-child {
+  button#menu-icon.close .bar:first-child {
     -webkit-transform: translateY(6px) rotate(45deg);
     -ms-transform: translateY(6px) rotate(45deg);
     transform: translateY(6px) rotate(45deg);
   }
-  div#menu-icon.close .bar:nth-child(2) {
+  button#menu-icon.close .bar:nth-child(2) {
     opacity: 0;
   }
-  div#menu-icon.close .bar:last-child {
+  button#menu-icon.close .bar:last-child {
     -webkit-transform: translateY(-6px) rotate(-45deg);
     -ms-transform: translateY(-6px) rotate(-45deg);
     transform: translateY(-6px) rotate(-45deg);
