3cabec45772d9e7c74437e957cbbde1debb3ba05
Michael Nelson <revnelson@gmail.com>
2023-04-26 07:46:36 +0000

    Modify MobileMenu to fix page when menu open.

--- astro/src/components/MobileMenu.svelte
+++ astro/src/components/MobileMenu.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import type { Collections } from "../utils/types/directus";
   import { isMenuOpen } from "../stores/menu";
 
   export let menuItems: Collections["menu_items"][] | null | undefined;
+  export let footerItems: Collections["footer_icons"][] | null | undefined;
 
   const clickOutside = (node: HTMLElement, ignore?: string) => {
     const handleClick = (event: Event) => {
@@ -24,26 +24,65 @@
       },
     };
   };
+
+  isMenuOpen.subscribe((open) => {
+    if (open) {
+      document.body.style.position = "fixed";
+      document.body.style.top = `-${window.scrollY}px`;
+    } else {
+      const scrollY = document.body.style.top;
+      document.body.style.position = "";
+      document.body.style.top = "";
+      window.scrollTo(0, parseInt(scrollY || "0") * -1);
+    }
+  });
 </script>
 
 <div
-  class={`absolute inset-0 max-w-[300px] bg-gradient-to-tr bg-opacity-[20%] from-slate-200 to-white bg-transparent ${
-    $isMenuOpen ? "" : "-translate-x-full"
-  } transition-all`}
-  use:clickOutside={"#user-menu-button"}
+  class={`md:hidden absolute inset-0 top-[128px] ${
+    $isMenuOpen ? "" : "-z-10 opacity-0"
+  } transition-all bg-gradient-to-tr from-slate-300 to-slate-50 duration-500`}
+  use:clickOutside={"#menu-icon"}
 >
   {#if menuItems}
-    <ul class="mt-16 bg-transparent">
+    <ul
+      class={`mt-16 ${
+        $isMenuOpen ? "" : "translate-y-16"
+      } transition-all duration-500`}
+    >
       {#each menuItems as menuItem}
-        <li
-          class="p-8 text-2xl text-center"
-          on:click={() => isMenuOpen.set(false)}
-          on:keypress={() => isMenuOpen.set(false)}
-        >
-          <a href={menuItem.path} class="py-8 tracking-wide">{menuItem.label}</a
-          >
+        <li class="p-8 text-2xl w-full text-center">
+          <a
+            href={menuItem.path}
+            on:click={() => isMenuOpen.set(false)}
+            on:keypress={() => isMenuOpen.set(false)}
+            class="w-full py-8 hover:text-highlight transition-colors"
+            >{menuItem.label}
+          </a>
         </li>
       {/each}
     </ul>
   {/if}
+  <div
+    class={`mt-28 ${
+      $isMenuOpen ? "" : "translate-y-16 opacity-0"
+    } transition-all delay-500 duration-700`}
+  >
+    <div class="social flex justify-center gap-4">
+      {#if footerItems}
+        {#each footerItems as icon}
+          <a
+            href={icon.link}
+            target="_blank"
+            class="hover:animate-pulse"
+            title={icon.brand}
+          >
+            <i
+              class={`fab fa-${icon.brand.toLowerCase()} fa-2x hover:animate-pulse`}
+            />
+          </a>
+        {/each}
+      {/if}
+    </div>
+  </div>
 </div>
