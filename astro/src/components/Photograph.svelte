4f1daa87f18652bcf57277ea445da51adaf47512
Michael Nelson <revnelson@gmail.com>
2023-04-26 07:43:53 +0000

    Add Photograph component.

---
+++ astro/src/components/Photograph.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { Lightbox } from "svelte-lightbox";
+
+  export let photograph: API_Photograph;
+
+  const apiURL = import.meta.env.PUBLIC_DIRECTUS_URL;
+</script>
+
+<Lightbox>
+  <img
+    src={`${apiURL}/assets/${photograph.image}`}
+    alt={photograph.alt}
+    class="object-contain rounded-sm md:rounded max-w-96 max-h-96"
+  />
+</Lightbox>
